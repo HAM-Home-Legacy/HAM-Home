e3eef2571035b1ff4cc79002f44a7813357a325c
Oussema sferi <62106784+oussema-sferi@users.noreply.github.com>
2020-07-09 18:41:06 +0100

    create post linked to db

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ require('dotenv').config();
 
 // Connection to DB
 const uri = process.env.URI;
-console.log(uri);
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useCreateIndex: true,
@@ -19,9 +18,23 @@ connection.once('open', () => {
   console.log('MongoDB connected');
 });
 
+app.use(express.json());
 app.use(express.static('client/dist'));
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
+//users Route
 app.use('/api/users', routes.userRoutes);
+
+//post Route
+app.use('/api/post', routes.postRoutes);
 
 app.get('*', (req, res) => {
   res.sendFile(__dirname + '/client/dist/index.html');
